0d4b16be4fa085afd0a3c67c7b6568f9fa15deb6
Felipe Mantoan <luisfelipemantoan@gmail.com>
2019-02-09 20:39:24 -0200

    Melhora um pouco a doxygen

--- src/Boleto/Boleto.php
+++ src/Boleto/Boleto.php
@@ -9,189 +9,265 @@ use DateTime;
  * @author Matheus Fidelis
  * @email matheus.fidelis@superlogica.com
  */
-class Boleto
-{
+class Boleto {
 
-    protected $storage = [];
+  /**
+   * Este parâmetro guarda os dados
+   * a fim de facilitar a coversão para array,
+   * além de facilitar a migração para um iterator.
+   * @var array
+   */
+  protected $storage = [];
 
-    /**
-     * Vencimento do boleto bancário.
-     * @var \DateTime
-     */
-    protected $vencimento;
+  /**
+   * Vencimento da cobrança no formato MM/DD/AAAA.
+   * @var \DateTime
+   */
+  protected $vencimento;
 
-    /**
-     * Valor total do boleto
-     * @var float
-     */
-    protected $valor;
+  /**
+   * Valor a ser cobrado em reais.
+   * Casas decimais devem ser separadas por ponto,
+   * máximo de 2 casas decimais, não enviar caracteres
+   * diferentes de número ou ponto.
+   * Não usar separadores de milhares.
+   *
+   * @var float
+   */
+  protected $valor;
 
-    /**
-     * Valor do juros cobrado no boleto.
-     * @var float
-     */
-    protected $juros;
+  /**
+   * Valor do juros cobrado no boleto.
+   * @var float
+   */
+  protected $juros;
 
-    /**
-     * Valor da multa cobrada em um boleto.
-     *
-     * @var float
-     */
-    protected $multa;
+  /**
+   * Valor da multa cobrada em um boleto.
+   *
+   * @var float
+   */
+  protected $multa;
 
-    /**
-     * Valor de um desconto.
-     *
-     * @var float
-     */
-    protected $desconto;
+  /**
+   * Valor de um desconto.
+   *
+   * @var float
+   */
+  protected $desconto;
 
-    /**
-     * Valor de um desconto.
-     *
-     * @var float
-     */
-    protected $desconto2;
+  /**
+   * Valor de um desconto.
+   *
+   * @var float
+   */
+  protected $desconto2;
 
-    /**
-     * Valor de um desconto.
-     *
-     * @var float
-     */
-    protected $desconto3;
+  /**
+   * Valor de um desconto.
+   *
+   * @var float
+   */
+  protected $desconto3;
 
-    /**
-     * Valor de um desconto.
-     *
-     * @var float
-     */
-    protected $diasDesconto;
+  /**
+   * Valor de um desconto.
+   *
+   * @var float
+   */
+  protected $diasDesconto;
 
-    /**
-     * Valor de um desconto.
-     *
-     * @var float
-     */
-    protected $diasDesconto2;
+  /**
+   * Valor de um desconto.
+   *
+   * @var float
+   */
+  protected $diasDesconto2;
 
-    /**
-     * Valor de um desconto.
-     *
-     * @var float
-     */
-    protected $diasDesconto3;
+  /**
+   * Valor de um desconto.
+   *
+   * @var float
+   */
+  protected $diasDesconto3;
 
-    /**
-     * Nome do cliente final.
-     *
-     * @var string
-     */
-    protected $nome_cliente;
+  /**
+   * Nome do cliente final.
+   *
+   * @var string
+   */
+  protected $nome_cliente;
 
-    /**
-     * CPF do cliente final.
-     * @var
-     */
-    protected $cpf_cliente;
+  /**
+   * CPF do cliente final.
+   * @var
+   */
+  protected $cpf_cliente;
 
-    /**
-     * Endereço do cliente final.
-     * @var
-     */
-    protected $endereco_cliente;
+  /**
+   * Endereço do cliente final.
+   * @var
+   */
+  protected $endereco_cliente;
 
-    /**
-     * Numero residencial do cliente final.
-     * @var
-     */
-    protected $numero_cliente;
+  /**
+   * Numero residencial do cliente final.
+   * @var
+   */
+  protected $numero_cliente;
 
-    /**
-     * Complemento opcional do endereço do cliente.
-     *
-     * @var string
-     */
-    protected $complemento_cliente;
+  /**
+   * Complemento opcional do endereço do cliente.
+   *
+   * @var string
+   */
+  protected $complemento_cliente;
 
-    /**
-     * Bairro do cliente final.
-     *
-     * @var
-     */
-    protected $bairro_cliente;
+  /**
+   * Bairro do cliente final.
+   *
+   * @var
+   */
+  protected $bairro_cliente;
 
-    /**
-     * Cidade do cliente final.
-     * @var
-     */
-    protected $cidade_cliente;
+  /**
+   * Cidade do cliente final.
+   * @var
+   */
+  protected $cidade_cliente;
 
-    /**
-     * Cidade do cliente final.
-     * @var
-     */
-    protected $cep_cliente;
+  /**
+   * Cidade do cliente final.
+   * @var
+   */
+  protected $cep_cliente;
 
-    /**
-     * Link do logo impresso no boleto.
-     * @var
-     */
-    protected $logo_url;
+  /**
+   * Link do logo impresso no boleto.
+   * @var
+   */
+  protected $logo_url;
 
-    /**
-     * Texto opcional do corpo do boleto.
-     * @var
-     */
-    protected $texto;
+  /**
+   * Texto opcional do corpo do boleto.
+   * @var
+   */
+  protected $texto;
 
-    /**
-     * Grupo
-     * @var
-     */
-    protected $grupo;
+  /**
+   * Identificação do grupo.
+   * É uma string que identifica um grupo de boletos.
+   * Quando um valor é passado neste campo, é retornado
+   * um link adicional para impressão de todos os boletos
+   * do mesmo grupo de uma vez.
+   * Recomendado para imprimir carnês.
+   * @var
+   */
+  protected $grupo;
 
-    /**
-     * Link do boleto
-     * @var
-     */
-    protected $link;
+  /**
+   * Link do boleto
+   * @var string
+   */
+  protected $link;
 
-    /**
-     * Nosso numero de boleto.
-     * @var
-     */
-    protected $nosso_numero;
+  /**
+   * Nosso numero de boleto.
+   * @var int
+   */
+  protected $nosso_numero;
 
-    /**
-     * Numero do pedido informado pelo cliente.
-     * @var
-     */
-    protected $pedido_numero;
+  /**
+   * Numero do pedido informado pelo cliente.
+   * @var string
+   */
+  protected $pedido_numero;
 
-    protected $linha_digitavel;
+  /**
+   * Opcionalmente informa a espécie do titulo da cobrança.
+   * @var string
+   */
+  protected $especie_documento;
 
-    protected $id_unico;
+  protected $linha_digitavel;
 
-    public function __set($property, $value) {
+  protected $id_unico;
 
-        if ($property == 'storage' || !property_exists($this, $property)) {
-            return ;
-        }
+  /**
+   * Opcionalmente informe uma URL de Webhook.
+   * Iremos chamá-la com as novas informações sempre que a cobrança for atualizada.
+   * @var string
+   */
+  protected $webhook;
 
-        $this->storage[strtolower($property)] = $this->$property = strip_tags($value);
+  /**
+   * Construtor básico.
+   *
+   * @param array $data
+   *   Dados que compoe o boleto.
+   */
+  public function __construct(array $data = [])
+  {
+
+    foreach ($data as $property => $value) {
+      $this->__set($property, $value);
+    }
+  }
+
+  /**
+   * Método mágico.
+   *
+   * @param string $property
+   *   O nome da propriedade.
+   * @param mixed $value
+   *   O valor da propriedade.
+   */
+  public function __set($property, $value)
+  {
+
+    $value = strip_tags($value);
+
+      // Não deixa o a propriedade storage ser setada.
+      // Verifica se a propriedade existe.
+    if ($property == 'storage' || !property_exists($this, $property)) {
+      return;
     }
 
-    public function __get($property) {
-        return $this->storage[$property] ?? null;
+      //Remove caracteres -(traço) e .(ponto).
+    if ($property == 'cpf_cliente' || $property == 'cep_cliente') {
+      $value = str_replace(['-', '.'], [''], $value);
     }
 
-    /**
-     * Pega os campos utilizados para a emissão do boleto bancário.
-     *
-     * @return array
-     */
-    public function toArray() : array {
-        return $this->storage;
+      // Converte o dado previamente.
+    if ($property == 'vencimento') {
+      $date = new DateTime($value);
+      $value = $date->format('m/d/Y');
     }
+
+    $this->storage[strtolower($property)] = $this->$property = $value;
+  }
+
+  /**
+   * Método mágico que retorna o valor de uma propriedade.
+   *
+   * @param string $property
+   *   A propriedade a ser retornada.
+   *
+   * @return mixed Valor da propriedade.
+   */
+  public function __get($property)
+  {
+    return $this->storage[$property] ?? $this->$property ?? null;
+  }
+
+  /**
+   * Pega os campos utilizados para a emissão do boleto bancário.
+   *
+   * @return array
+   */
+  public function toArray() : array
+  {
+    return $this->storage;
+  }
 
 }
