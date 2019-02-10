259932326a55a3109820cd0904079b4320176962
Felipe Mantoan <luisfelipemantoan@gmail.com>
2019-02-10 13:40:59 -0200

    Corrige identação

--- src/Client/PJBankInterface.php
+++ src/Client/PJBankInterface.php
@@ -2,36 +2,41 @@
 
 namespace PJBank\Client;
 
-use Exception;
-use GuzzleHttp\Client;
-use GuzzleHttp\ClientInterface;
-use GuzzleHttp\RequestOptions;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Handler\CurlHandler;
-
 /**
  * Client Factory
  */
-interface PJBankInterface
-{
-    /**
-     * URI base da API.
-     * @var string
-     */
-    const API_URI = "https://api.pjbank.com.br/";
+interface PJBankInterface {
 
-    /**
-     * URI base da API Sandbox.
-     * @var string
-     */
-    const SANDBOX_URI = "https://sandbox.pjbank.com.br/";
+  /**
+   * URI base da API.
+   * @var string
+   */
+  const API_URI = "https://api.pjbank.com.br/";
 
-    public function sendPut(string $endpoint, array $data = []);
+  /**
+   * URI base da API Sandbox.
+   * @var string
+   */
+  const SANDBOX_URI = "https://sandbox.pjbank.com.br/";
 
-    public function sendDelete(string $endpoint, array $data = []);
+  /**
+   * Método envia um put usando a credencial e chave.
+   */
+  public function sendPut(string $endpoint, array $data = []);
 
-    public function sendPost(string $endpoint, array $data = [], bool $withKey = true);
+  /**
+   * Método envia um delete usando a credencial e chave.
+   */
+  public function sendDelete(string $endpoint, array $data = []);
 
-    public function sendGet(string $endpoint, array $data = []);
+  /**
+   * Método envia um post usando a credencial e chave.
+   */
+  public function sendPost(string $endpoint, array $data = [], bool $withKey = true);
+
+  /**
+   * Método envia um get usando a credencial e chave.
+   */
+  public function sendGet(string $endpoint, array $data = []);
 
 }
