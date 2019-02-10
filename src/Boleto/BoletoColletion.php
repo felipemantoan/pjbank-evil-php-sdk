9795dde314fcfd166f954e61da3ed55a8a3fafab
Felipe Mantoan <luisfelipemantoan@gmail.com>
2019-02-10 14:08:11 -0200

    Corrige comentários

--- src/Boleto/BoletoColletion.php
+++ src/Boleto/BoletoColletion.php
@@ -5,67 +5,75 @@ namespace PJBank\Boleto;
 use Iterator;
 
 /**
- * Boleto Registrado
- * @author Matheus Fidelis
- * @email matheus.fidelis@superlogica.com
+ * Lote de boletos.
  */
 class BoletoColletion implements Iterator
 {
 
-    protected $storage = [];
+  protected $storage = [];
 
-    public function add(Boleto $boleto) {
-        $this->storage['cobrancas'][$boleto->pedido_numero] = $boleto;
-        return $this;
+  public function add(Boleto $boleto)
+  {
+    $this->storage['cobrancas'][$boleto->pedido_numero] = $boleto;
+    return $this;
+  }
+
+  public function addItems(array $boletos = [])
+  {
+    foreach ($boletos as $boleto) {
+      $this->add($boleto);
     }
+    return $this;
+  }
 
-    public function addItems(array $boletos = []) {
-        foreach ($boletos as $boleto) {
-            $this->add($boleto);
-        }
-        return $this;
+  public function get($index)
+  {
+    return $this->storage['cobrancas'][$index] ?? null;
+  }
+
+  public function getItems()
+  {
+    return $this->storage['cobrancas'] ?? [];
+  }
+
+  public function rewind()
+  {
+    return reset($this->storage['cobrancas']);
+  }
+
+
+  public function next()
+  {
+    return next($this->storage['cobrancas']);
+  }
+
+  public function current()
+  {
+    return current($this->storage['cobrancas']);
+  }
+
+  public function key()
+  {
+    return key($this->storage['cobrancas']);
+  }
+
+  public function valid()
+  {
+    return !empty(key($this->storage['cobrancas']));
+  }
+
+  /**
+   * Pega os campos utilizados para a emissão do boleto bancário.
+   *
+   * @return array
+   */
+  public function toArray() : array
+  {
+    $collection = [];
+    foreach ($this->storage['cobrancas'] as $boleto) {
+      $collection['cobrancas'][] = $boleto->toArray();
     }
-
-    public function get($index) {
-        return $this->storage['cobrancas'][$index] ?? null;
-    }
-
-    public function getItems() {
-        return $this->storage['cobrancas'] ?? [];
-    }
-
-    public function rewind() {
-        return reset($this->storage['cobrancas']);
-    }
-
-
-    public function next() {
-        return next($this->storage['cobrancas']);
-    }
-
-    public function current() {
-        return current($this->storage['cobrancas']);
-    }
-
-    public function key() {
-        return key($this->storage['cobrancas']);
-    }
-
-    public function valid() {
-        return !empty(key($this->storage['cobrancas']));
-    }
-
-    /**
-     * Pega os campos utilizados para a emissão do boleto bancário.
-     *
-     * @return array
-     */
-    public function toArray() : array {
-        $collection = [];
-        foreach ($this->storage['cobrancas'] as $boleto) {
-            $collection['cobrancas'][] = $boleto->toArray();
-        }
-        return $collection;
-    }
+    return $collection;
+  }
 
 }
